52b66e905df8cd4189f80986cd4d7046434ce611
hanyichen609 <hanyichen609@gmail.com>
2025-01-08 15:52:26 +0800

    Add files via upload

---
+++ import turtle.py
@@ -0,0 +1,35 @@
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+t.hideturtle()  # 隐藏海龟图标
+
+for i in range(100):
+    t.fd(50)
+    t.lt(25)
+    t.circle(20)
+t.lt(20)
+
+for i in range(100):
+    t.fd(50)
+    t.lt(25)
+    t.circle(20)
+t.lt(20)
+
+for i in range(100):
+    t.fd(50)
+    t.lt(25)
+    t.circle(20)
+t.lt(20)
+
+for i in range(100):
+    t.fd(50)
+    t.lt(25)
+    t.circle(20)
+t.lt(20)
+
+for i in range(100):
+    t.fd(50)
+    t.lt(25)
+    t.circle(20)
+
+turtle.done()
